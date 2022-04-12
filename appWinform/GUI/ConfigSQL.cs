bafef2e647c21ad02708ec5f11f53573da832b9c
LDT-HiHiHiHi <LDT-HiHiHiHi>
2022-04-12 13:58:23 +0700

    form cau hinh , bao cao word, diagram

---
+++ appWinform/GUI/ConfigSQL.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class ConfigSQL : UserControl
+    {
+        public ConfigSQL()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHuy_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtUser.Text.Trim()))
+            {
+                MessageBox.Show("Vui lòng nhập UserName !", "Thông báo");
+                txtUser.Focus();
+                return;
+            }
+
+            if (string.IsNullOrEmpty(txtPass.Text.Trim()))
+            {
+                MessageBox.Show("Vui lòng nhập Password !", "Thông báo");
+                txtPass.Focus();
+                return;
+            }
+
+        }
+    }
+}
